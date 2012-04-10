821c44f7b5e42732c886c2bfb2ef9331777f7633
Anton Keks <anton@codeborne.com>
2012-04-10 14:54:39 +0300

    tests updated to source changes, incomplete yet

--- test/com/codeborne/security/mobileid/MobileIDAuthenticatorTest.java
+++ test/com/codeborne/security/mobileid/MobileIDAuthenticatorTest.java
@@ -2,6 +2,7 @@ package com.codeborne.security.mobileid;
 
 import com.codeborne.security.AuthenticationException;
 import com.codeborne.security.digidoc.DigiDocServicePortType;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
@@ -11,7 +12,7 @@ import javax.xml.rpc.holders.StringHolder;
 import java.rmi.RemoteException;
 
 import static java.lang.String.valueOf;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
@@ -21,7 +22,7 @@ public class MobileIDAuthenticatorTest {
 
   @Test
   public void startSession() throws RemoteException, AuthenticationException {
-    mid.digiDocServicePortType = mockMobileIdAuthenticator("OK", "Bruce", "Willis", "38105060708", "4567", 123);
+    mid.service = mockAuthentication("OK", "Bruce", "Willis", "38105060708", "4567", 123);
 
     MobileIDSession session = mid.startLogin("+37255667788");
     assertThat(session.firstName, equalTo("Bruce"));
@@ -33,11 +34,27 @@ public class MobileIDAuthenticatorTest {
 
   @Test(expected = AuthenticationException.class)
   public void throwsExceptionIfError() throws RemoteException, AuthenticationException {
-    mid.digiDocServicePortType = mockMobileIdAuthenticatorError(100);
+    mid.service = mockError(100);
     mid.startLogin("+37255667788");
   }
 
-  private DigiDocServicePortType mockMobileIdAuthenticator(final String result, final String firstName, final String lastName, final String personalCode, final String challenge, final int sessCode) throws RemoteException {
+  @Test @Ignore
+  public void waitsIfStatusOutstandingTransaction() throws Exception {
+    mid.service = mockStatus("OUTSTANDING_TRANSACTION");
+  }
+
+  private DigiDocServicePortType mockStatus(final String status) throws RemoteException {
+    DigiDocServicePortType service = mock(DigiDocServicePortType.class);
+    doAnswer(new Answer<Object>() {
+      @Override public Object answer(InvocationOnMock invocation) throws Throwable {
+        ((StringHolder)invocation.getArguments()[2]).value = status;
+        return null;
+      }
+      }).when(service).getMobileAuthenticateStatus(anyInt(), eq(false), any(StringHolder.class), any(StringHolder.class));
+    return service;
+  }
+
+  private DigiDocServicePortType mockAuthentication(final String result, final String firstName, final String lastName, final String personalCode, final String challenge, final int sessCode) throws RemoteException {
     DigiDocServicePortType service = mock(DigiDocServicePortType.class);
     doAnswer(new Answer<Object>() {
       @Override
@@ -58,7 +75,7 @@ public class MobileIDAuthenticatorTest {
     return service;
   }
 
-  private DigiDocServicePortType mockMobileIdAuthenticatorError(int errorCode) throws RemoteException {
+  private DigiDocServicePortType mockError(int errorCode) throws RemoteException {
     DigiDocServicePortType service = mock(DigiDocServicePortType.class);
     doThrow(new RemoteException(valueOf(errorCode)))
     .when(service).mobileAuthenticate(anyString(), anyString(), eq("37255667788"), anyString(),
